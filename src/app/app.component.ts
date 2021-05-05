0961cc9d3dc4a03b50e66a22541cef8b2a93ce73
precodepath <precodepath@gmail.com>
2021-05-05 08:32:57 +0530

    create angular complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
+/**
+ * dasdsad
+ * sdsadsad
+ */
+@Component(
+  {
+    selector: 'myroot-compunent', // <app-root></app-root>
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'] // [1,2,3,4]
+  }
+  )
 export class AppComponent {
-  title = 'basic-angular';
+  public title = 'basic-angular';
+
+   constructor() {
+    // this.title = first;
+   }
+
 }
+
+
+// let apc1 = new AppComponent();
+
+// let apc2 = new AppComponent();
+
+// let apc3 = new AppComponent();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductPriceComponent } from './product-price/product-price.component';
+import { ProductCardComponent } from './product-card/product-card.component';
+import { ProductDescComponent } from './product-desc/product-desc.component';
+import { ProductTagComponent } from './product-tag/product-tag.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductPriceComponent,
+    ProductCardComponent,
+    ProductDescComponent,
+    ProductTagComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
