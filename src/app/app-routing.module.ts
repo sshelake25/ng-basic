29681aa2103d6ec5079da30484f1aee4a668cdb2
precodepath <precodepath@gmail.com>
2021-05-22 07:39:06 +0530

    routes configration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { LearnIfComponent } from './learn-if/learn-if.component';
+import { ProductCardComponent } from './product-card/product-card.component';
+import { ProductPriceComponent } from './product-price/product-price.component';
+import { ReactiveformBasicComponent } from './reactiveform-basic/reactiveform-basic.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProductCardComponent },
+  { path: 'product-list', component: LearnIfComponent },
+  { path: 'person', component: AdminComponent },
+  { path: 'find', redirectTo: 'search' },
+  { path: 'search', component: ReactiveformBasicComponent },
+  { path: '**', component: ProductPriceComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
